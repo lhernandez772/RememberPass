d77d2c63f2b79acc55c5aba3ebd84dc622cd575c
Luish000 <luish000.lh@gmail.com>
2013-11-06 07:37:56 -0430

    Prueba

---
+++ dontForgetPass/src/main/java/com/example/dontforgetpass/PasswordDialog.java
@@ -0,0 +1,10 @@
+package com.example.dontforgetpass;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by lhernandez on 03/11/13.
+ */
+public class PasswordDialog extends DialogFragment {
+
+}
